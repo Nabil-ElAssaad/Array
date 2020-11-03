0f62c039be47cdae53f4bb2fe64c26e4bd46950b
Nabil-ElAssaad <72812191+Nabil-ElAssaad@users.noreply.github.com>
2020-11-03 15:06:29 -0500

    Add files via upload

---
+++ maxArr.java
@@ -0,0 +1,12 @@
+public class maxArr {
+    public static void main(String[] args) {
+        int [] m = {1,2,55,3,4,5,6,7};
+        int max =0;
+        for (int i = 0 ; i < m.length ; i++){
+            if (m[i] > max) {
+                max = m[i];
+            }}
+
+            System.out.print(max);
+    }
+}
