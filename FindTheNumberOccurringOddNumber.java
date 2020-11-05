ac786b00a5a3bd0e5ac18afd7338a656ec4aee0d
Nabil-ElAssaad <72812191+Nabil-ElAssaad@users.noreply.github.com>
2020-11-05 12:29:47 -0500

    Add files via upload

---
+++ FindTheNumberOccurringOddNumber.java
@@ -0,0 +1,14 @@
+package intetviewQ;
+
+public class FindTheNumberOccurringOddNumber {
+    public static void main(String[] args) {
+        int [] numbers =   {2,3 , 7 , 8 , 10 } ;
+        for (int i = 0 ; i < numbers.length ; i++ ) {
+          
+            }
+        }
+
+    }
+
+    }
+
